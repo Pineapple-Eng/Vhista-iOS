9259d022aeec910b67be60b9698b6d87bae91d7d
David Cruz <juandavidcruzs@gmail.com>
2019-06-29 19:41:25 -0700

    Adding Features

---
+++ Vhista/Feature.swift
@@ -0,0 +1,14 @@
+//
+//  Feature.swift
+//  Vhista
+//
+//  Created by Juan David Cruz Serrano on 6/27/19.
+//  Copyright © 2019 juandavidcruz. All rights reserved.
+//
+
+import Foundation
+
+struct Feature {
+    var logoView: LogoView
+    var featureName: String
+}
---
+++ Vhista/FeaturesCollectionViewController.swift
@@ -0,0 +1,77 @@
+//
+//  FeaturesCollectionViewController.swift
+//  Vhista
+//
+//  Created by Juan David Cruz Serrano on 6/27/19.
+//  Copyright © 2019 juandavidcruz. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "FeatureCell"
+
+class FeaturesCollectionViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Register cell classes
+        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+    }
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return 0
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+    
+        // Configure the cell
+    
+        return cell
+    }
+
+    // MARK: UICollectionViewDelegate
+
+    /*
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+    
+    }
+    */
+
+}
