c22cdea574457464c7a6e85dffcb4417de1d41cd
David Cruz <juandavidcruzs@gmail.com>
2019-07-27 08:40:27 -0700

    Create RecognizedContentViewController

---
+++ Vhista/UI/BottomView/RecognizedContentViewController.swift
@@ -0,0 +1,15 @@
+//
+//  RecognizedContentViewController.swift
+//  Vhista
+//
+//  Created by Juan David Cruz Serrano on 7/27/19.
+//  Copyright © Juan David Cruz Serrano & Vhista Inc. All rights reserved.
+//
+
+import UIKit
+
+class RecognizedContentViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
